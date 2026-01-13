8d4819ed57ab9330085d87387a12da56e541e3f5
GRPS GANEGODA <piumisarangag@gmail.com>
2026-01-13 21:05:01 +0530

    changed the docker path of docker file

--- src/main.py
+++ src/main.py
@@ -1,6 +1,5 @@
 from fastapi import FastAPI
 from src.app.api.chat import router as chat_router
-
 app = FastAPI(title="Chatbot API")
 
 app.include_router(chat_router, prefix="/chat", tags=["chat"])
